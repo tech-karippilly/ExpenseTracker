cc13827c361e3da0dffda887ef72cb8d670fe32a
“tech-karippilly” <vishnukarippilly@gmail.com>
2024-09-06 19:54:39 +0530

    Added Splash Screen

--- expenss_front_end/src/Navigation/Stack/StackNavigation.tsx
+++ expenss_front_end/src/Navigation/Stack/StackNavigation.tsx
@@ -1,10 +1,13 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../../Screens/HomeScreen";
+import SplashScreen from "../../Screens/HomeScreen/SplashScreen/Index";
 
 const Stack = createNativeStackNavigator();
 
 export const StackNavigation = () => {
-    return <Stack.Navigator>
+    return (
+    <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen name="SplashScreen" component={SplashScreen}/>
         <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    </Stack.Navigator>)
 }
---
+++ expenss_front_end/src/Screens/HomeScreen/SplashScreen/Index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ParentWrapper from '../../../Components/ParentWrapper';
+import { Text, View } from 'react-native';
+
+
+const SplashScreen = () =>{
+    return(
+        <ParentWrapper>
+            <View>
+                <Text>Splash Screen</Text>
+            </View>
+        </ParentWrapper>
+    )
+}
+
+export default SplashScreen;
